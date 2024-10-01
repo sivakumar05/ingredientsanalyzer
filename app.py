21dff37f52054ed8a317746dd7b9332fb1b3e907
Sivakumar Menta <sivakumar.menta@gmail.com>
2024-10-01 19:26:50 +0530

    secrets updated

--- app.py
+++ app.py
@@ -7,31 +7,38 @@ References:
 '''
 
 import streamlit as st
-import os
-from openai import OpenAI
+import os 
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 
-#from langchain_community import llms
-#from langchain.llms import OpenAI
+from openai import OpenAI
+import configparser
+
+config = configparser.RawConfigParser()
+config.read('config.ini')
+    
+details_dict = dict(config.items('My Section'))
+print(details_dict)
+KVUri = f"https://openai-ia.vault.azure.net"
+
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+
+retrieved_secret = client.get_secret('openai')
+
 
 # Setting the title of the Streamlit application
 st.title('Product Ingredients Analyzer')
 
 # Creating a sidebar input widget for the OpenAI API key, input type is password for security
 
-apikey='sk-36zXGcu_tJqTPzrKRi6G2UXF5FUPjeDaltTjBc6-ajT3BlbkFJBdr4EughftGeArWidxKP5Sdss3cTmC84uXJm2d_4kA'
-#st.sidebar.text_input('OpenAI API Key', type='password')
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=apikey,
-)
-# Defining a function to generate a response using the OpenAI model
-def generate_response(input_text):
-    # Initializing the OpenAI model with a specified temperature and API key
-    llm = OpenAI(temperature=0.0, openai_api_key=openai.api_key)
-    # Displaying the generated response as an informational message in the Streamlit app
-   
-    st.info(llm(prompt))
 
+#st.sidebar.text_input('OpenAI API Key', type='password')
+
+
+client = OpenAI(
+    api_key=retrieved_secret.value
+    )
 
     
 # Creating a form in the Streamlit app for user input
@@ -71,25 +78,21 @@ with st.form('my_form'):
             temperature=0 # this is the degree of randomness of the model's output
              
         )
-        #return response.choices[0].message["content"]
+        
         st.info(response.choices[0].message.content)
         
 
 
 
     # Displaying a warning if the entered API key does not start with 'sk-'
-    if not apikey.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
     # If the form is submitted and the API key is valid, generate a response
-    if submitted and apikey.startswith('sk-'):
-        #generate_response(prompt)
-        #print(get_completion(prompt, model="gpt-3.5-turbo"))
+    if submitted :
         get_completion(prompt, model="gpt-4")
     if click_clear:
     
-        a=1 
+        pass 
     
     if click_product_ingredients:
     
-        a=1
+        pass
     
